2a521ee700174c2a9cc766ea10ee809e32e7cb1e
Santhosh V <santhoshvr35@gmail.com>
2022-06-25 22:21:53 +0100

    updatecheck1

--- src/main/java/com/todo/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/todo/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -10,5 +10,5 @@ public class RestfulWebServicesApplication extends SpringBootServletInitializer 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
-
+//Santhosh V
 }
